7a89195f512addb2e58b06f337175eab5e10ca8b
Denys Haryachyy <denysh@flexiwan.com>
2020-02-06 07:56:19 +0200

    Removed redundant constants.

--- fwtranslate_add_app.py
+++ fwtranslate_add_app.py
@@ -22,6 +22,7 @@
 
 import os
 import re
+import ctypes
 
 import fwglobals
 import fwtranslate_revert
@@ -52,42 +53,13 @@ import fwutils
 #            }]
 # }
 
-# traffic types
-IP = 0
-ICMP = 1
-
-# IP version
-IPRANDOM = -1
-IPV4 = 0
-IPV6 = 1
-
-# rule types
-DENY = 0
-PERMIT = 1
-
-# supported protocols
-proto = [[6, 17], [1, 58]]
-proto_map = {1: 'ICMP', 58: 'ICMPv6EchoRequest', 6: 'TCP', 17: 'UDP'}
-ICMPv4 = 0
-ICMPv6 = 1
-TCP = 0
-UDP = 1
-PROTO_ALL = 0
-
-# port ranges
-PORTS_ALL = -1
-PORTS_RANGE = 0
-
-# ACL
-NEW_ACL_ID = 4294967295
-
-def _create_rule(ip=0, permit_deny=0, proto=0,
+def _create_rule(is_ipv6=0, is_permit=0, proto=0,
                  sport_from=0, sport_to=65535,
                  s_prefix=0, s_ip='\x00\x00\x00\x00',
                  dport_from=0, dport_to=65535,
                  d_prefix=0, d_ip='\x00\x00\x00\x00'):
 
-    rule = ({'is_permit': permit_deny, 'is_ipv6': ip, 'proto': proto,
+    rule = ({'is_permit': is_permit, 'is_ipv6': is_ipv6, 'proto': proto,
              'srcport_or_icmptype_first': sport_from,
              'srcport_or_icmptype_last': sport_to,
              'src_ip_prefix_len': s_prefix,
@@ -112,14 +84,14 @@ def _add_acl(params, cmd_list):
     for rule in params['rules']:
         ip_bytes, ip_len = fwutils.ip_str_to_bytes(rule['ip'])
 
-        rules.append(_create_rule(ip=IPV4, permit_deny=PERMIT,
+        rules.append(_create_rule(is_ipv6=0, is_permit=1,
                                   dport_from=rule['port-range-low'],
                                   dport_to=rule['port-range-high'],
                                   d_prefix=rule['ip-prefix'],
                                   d_ip=ip_bytes))
 
     cmd_params = {
-            'acl_index'        : NEW_ACL_ID,
+            'acl_index'        : ctypes.c_uint(-1).value,
             'count'            : len(rules),
             'r'                : rules,
             'tag'              : ''
