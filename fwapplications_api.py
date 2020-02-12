efa2dc9d7c7e7a55bf078861a976311149e6b7d4
Denys Haryachyy <denysh@flexiwan.com>
2020-02-12 22:58:39 +0200

    Create VPP ACL inside a wrapper.

--- fwapplications_api.py
+++ fwapplications_api.py
@@ -20,9 +20,11 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 ################################################################################
 
+import ctypes
 import os
 import re
 import time
+import fwutils
 from collections import defaultdict
 
 import fwglobals
@@ -41,6 +43,77 @@ class FwApps:
         """
         def tree(): return defaultdict(tree)
         self.apps_map = tree()
+
+    def _create_rule(self, is_ipv6=0, is_permit=0, proto=0,
+                     sport_from=0, sport_to=65535,
+                     s_prefix=0, s_ip='\x00\x00\x00\x00',
+                     dport_from=0, dport_to=65535,
+                     d_prefix=0, d_ip='\x00\x00\x00\x00'):
+
+        rule = ({'is_permit': is_permit, 'is_ipv6': is_ipv6, 'proto': proto,
+                 'srcport_or_icmptype_first': sport_from,
+                 'srcport_or_icmptype_last': sport_to,
+                 'src_ip_prefix_len': s_prefix,
+                 'src_ip_addr': s_ip,
+                 'dstport_or_icmpcode_first': dport_from,
+                 'dstport_or_icmpcode_last': dport_to,
+                 'dst_ip_prefix_len': d_prefix,
+                 'dst_ip_addr': d_ip})
+        return rule
+
+    def _add_acl(self, params, cmd_list):
+        """Generate ACL command.
+
+         :param params:        Parameters from flexiManage.
+         :param cmd_list:      Commands list.
+
+         :returns: None.
+         """
+        # acl.api.json: acl_add_replace (..., tunnel <type vl_api_acl_rule_t>, ...)
+        rules = []
+
+        for rule in params['rules']:
+            ip_bytes, ip_len = fwutils.ip_str_to_bytes(rule['ip'])
+
+            rules.append(self._create_rule(is_ipv6=0, is_permit=1,
+                                           dport_from=rule['port-range-low'],
+                                           dport_to=rule['port-range-high'],
+                                           d_prefix=rule['ip-prefix'],
+                                           d_ip=ip_bytes))
+
+        cmd_params = {
+            'acl_index': ctypes.c_uint(-1).value,
+            'count': len(rules),
+            'r': rules,
+            'tag': ''
+        }
+
+        cmd = {}
+        cmd['cmd'] = {}
+        cmd['cmd']['name'] = "acl_add_replace"
+        cmd['cmd']['params'] = cmd_params
+        cmd['cmd']['descr'] = "Add ACL for app %s" % (params['app'])
+        cmd_list.append(cmd)
+
+    def _remove_acl(self, acl_index, cmd_list):
+        """Generate delete ACL command.
+
+         :param acl_index:     ACL index.
+         :param cmd_list:      Commands list.
+
+         :returns: None.
+         """
+        # acl.api.json: acl_del (..., acl_index, ...)
+
+        cmd_params = {
+            'acl_index': acl_index
+        }
+
+        cmd = {}
+        cmd['cmd'] = {}
+        cmd['cmd']['name'] = "acl_del"
+        cmd['cmd']['params'] = cmd_params
+        cmd_list.append(cmd)
 
     def call(self, req, params):
         """Invokes API specified by the 'req' parameter.
@@ -69,12 +142,18 @@ class FwApps:
         :returns: Reply.
         """
         name = params['app']
-        acl_id = params['acl_index']
         category = params['category']
         subcategory = params['subcategory']
         priority = params['priority']
 
-        self.apps_map[category][subcategory][priority][name] = {'acl_id': acl_id}
+        cmd_list = []
+        cmd_cache = {}
+        self._add_acl(params, cmd_list)
+        for cmd in cmd_list:
+            result = {'result_attr': 'acl_index', 'cache': cmd_cache, 'key': 'acl_index'}
+            fwglobals.g.handle_request(cmd['cmd']['name'], cmd['cmd']['params'], result)
+            acl_id = cmd_cache['acl_index']
+            self.apps_map[category][subcategory][priority][name] = {'acl_id': acl_id}
 
         fwglobals.g.policy_api.refresh_policies()
 
@@ -89,12 +168,16 @@ class FwApps:
         :returns: Reply.
         """
         name = params['app']
-        acl_id = params['acl_index']
         category = params['category']
         subcategory = params['subcategory']
         priority = params['priority']
+        acl_id = self.apps_map[category][subcategory][priority][name]['acl_id']
+        cmd_list = []
 
-        del self.apps_map[category][subcategory][priority][name]
+        self._remove_acl(acl_id, cmd_list)
+        for cmd in cmd_list:
+            fwglobals.g.handle_request(cmd['cmd']['name'], cmd['cmd']['params'])
+            del self.apps_map[category][subcategory][priority][name]
 
         reply = {'ok': 1}
         return reply
--- fwtranslate_add_app.py
+++ fwtranslate_add_app.py
@@ -23,6 +23,7 @@
 import os
 import re
 import ctypes
+import copy
 
 import fwglobals
 import fwtranslate_revert
@@ -52,94 +53,6 @@ import fwutils
 #              "port-range-high":53}]
 #            }]
 # }
-
-def _create_rule(is_ipv6=0, is_permit=0, proto=0,
-                 sport_from=0, sport_to=65535,
-                 s_prefix=0, s_ip='\x00\x00\x00\x00',
-                 dport_from=0, dport_to=65535,
-                 d_prefix=0, d_ip='\x00\x00\x00\x00'):
-
-    rule = ({'is_permit': is_permit, 'is_ipv6': is_ipv6, 'proto': proto,
-             'srcport_or_icmptype_first': sport_from,
-             'srcport_or_icmptype_last': sport_to,
-             'src_ip_prefix_len': s_prefix,
-             'src_ip_addr': s_ip,
-             'dstport_or_icmpcode_first': dport_from,
-             'dstport_or_icmpcode_last': dport_to,
-             'dst_ip_prefix_len': d_prefix,
-             'dst_ip_addr': d_ip})
-    return rule
-
-def _add_acl(params, cmd_list):
-    """Generate ACL command.
-
-     :param params:        Parameters from flexiManage.
-     :param cmd_list:      Commands list.
-
-     :returns: Cache key of ACL id.
-     """
-    # acl.api.json: acl_add_replace (..., tunnel <type vl_api_acl_rule_t>, ...)
-    rules = []
-
-    for rule in params['rules']:
-        ip_bytes, ip_len = fwutils.ip_str_to_bytes(rule['ip'])
-
-        rules.append(_create_rule(is_ipv6=0, is_permit=1,
-                                  dport_from=rule['port-range-low'],
-                                  dport_to=rule['port-range-high'],
-                                  d_prefix=rule['ip-prefix'],
-                                  d_ip=ip_bytes))
-
-    cmd_params = {
-            'acl_index'        : ctypes.c_uint(-1).value,
-            'count'            : len(rules),
-            'r'                : rules,
-            'tag'              : ''
-    }
-
-    ret_attr = 'acl_index'
-    cache_key = 'acl_index'
-    cmd = {}
-    cmd['cmd'] = {}
-    cmd['cmd']['name']          = "acl_add_replace"
-    cmd['cmd']['params']        = cmd_params
-    cmd['cmd']['cache_ret_val'] = (ret_attr, cache_key)
-    cmd['cmd']['descr']         = "Add ACL for app %s" % (params['app'])
-    cmd['revert'] = {}
-    cmd['revert']['name']       = 'acl_del'
-    cmd['revert']['params']     = {'substs': [ { 'add_param':ret_attr, 'val_by_key':cache_key} ]}
-    cmd['revert']['descr']      = "Delete ACL for app %s" % (params['app'])
-    cmd_list.append(cmd)
-
-    return cache_key
-
-def _add_app(params, cache_key, cmd_list):
-    """Generate APP in DB.
-
-     :param params:        Parameters from flexiManage.
-     :param cache_key:     ACL id cache key.
-     :param cmd_list:      Commands list.
-
-     """
-    cmd_params = {
-            'substs': [ { 'add_param':cache_key, 'val_by_key':cache_key} ],
-            'app': params['app'],
-            'id': params['id'],
-            'category': params['category'],
-            'subcategory': params['subcategory'],
-            'priority': params['priority']
-    }
-    cmd = {}
-    cmd['cmd'] = {}
-    cmd['cmd']['name']          = "add-app-info"
-    cmd['cmd']['params']        = cmd_params
-    cmd['cmd']['descr']         = "Add APP %s" % (params['app'])
-    cmd['revert'] = {}
-    cmd['revert']['name']       = 'remove-app-info'
-    cmd['revert']['params']     = cmd_params
-    cmd['revert']['descr']      = "Delete APP %s" % (params['app'])
-    cmd_list.append(cmd)
-
 def add_app(params):
     """Generate App commands.
 
@@ -149,8 +62,16 @@ def add_app(params):
     """
     cmd_list = []
 
-    acl_id_cache_key = _add_acl(params, cmd_list)
-    _add_app(params, acl_id_cache_key, cmd_list)
+    cmd = {}
+    cmd['cmd'] = {}
+    cmd['cmd']['name']          = "add-app-info"
+    cmd['cmd']['params']        = copy.deepcopy(params)
+    cmd['cmd']['descr']         = "Add APP %s" % (params['app'])
+    cmd['revert'] = {}
+    cmd['revert']['name']       = 'remove-app-info'
+    cmd['revert']['params']     = copy.deepcopy(params)
+    cmd['revert']['descr']      = "Delete APP %s" % (params['app'])
+    cmd_list.append(cmd)
 
     return cmd_list
 
