00d4f42b3a452ee43ac6d7e9bbfd69cce88e901d
IgorN <igor@flexiwan.com>
2020-08-23 11:46:22 +0300

    Add log prints to daemon_rpc()

--- fwagent.py
+++ fwagent.py
@@ -985,8 +985,10 @@ def daemon_rpc(func, **kwargs):
         fwglobals.log.debug("invoke remote FwagentDaemon::%s(%s)" % (func, json.dumps(kwargs)))
         return remote_func(**kwargs)
     except Pyro4.errors.CommunicationError:
+        fwglobals.log.debug("no FwagentDaemon was detected")
         return None
-    except:
+    except Exception as e:
+        fwglobals.log.debug("failed to connect to FwagentDaemon: %s" % str(e))
         ex_type, ex_value, ex_tb = sys.exc_info()
         Pyro4.util.excepthook(ex_type, ex_value, ex_tb)
         return None
