8c0275076d6b41100fb3c138c60f6332d5051d74
Denys Haryachyy <denysh@flexiwan.com>
2020-10-06 08:40:30 +0000

    If VPP is running after reboot cleanup Netplan files first.

--- fwrouter_api.py
+++ fwrouter_api.py
@@ -201,13 +201,12 @@ class FWROUTER_API:
             fwglobals.log.debug("restore_vpp_if_needed: no need to restore(vpp_runs=%s, vpp_should_be_started=%s)" %
                 (str(vpp_runs), str(vpp_should_be_started)))
             self.router_started = vpp_runs
+            fwnetplan.restore_linux_netplan_files()
             if self.router_started:
                 fwglobals.log.debug("restore_vpp_if_needed: vpp_pid=%s" % str(fwutils.vpp_pid()))
                 self._start_threads()
                 netplan_files = fwnetplan.get_netplan_filenames()
                 fwnetplan._set_netplan_filename(netplan_files)
-            else:
-                fwnetplan.restore_linux_netplan_files()
             return False
 
         self._restore_vpp()
