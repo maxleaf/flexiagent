4fb1b81eff472bc8c2571a1da7ac345433c9ccd6
shneor <shneorp@flexiwan.com>
2020-11-25 08:12:18 +0000

    remove critical from netplan

--- fwnetplan.py
+++ fwnetplan.py
@@ -236,7 +236,7 @@ def add_remove_netplan_interface(is_add, dev_id, ip, gw, metric, dhcp, type):
 
             config_section['dhcp4'] = True
             config_section['dhcp4-overrides'] = {'route-metric': metric}
-            config_section['critical'] = True   # Prevent lease release on networkd restart or no answer from DHCP server
+            # config_section['critical'] = True   # Prevent lease release on networkd restart or no answer from DHCP server
         else:
             config_section['dhcp4'] = False
             if 'dhcp4-overrides' in config_section:
