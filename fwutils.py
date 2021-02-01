b814345caa40a644f1f507ec8d88d52b1cd21f03
Denys Haryachyy <denysh@flexiwan.com>
2021-02-01 14:21:11 +0000

    Fixed vxlan create error on unicode.

--- fwutils.py
+++ fwutils.py
@@ -238,7 +238,7 @@ def get_interface_gateway(if_name, if_pci=None):
 
 def get_binary_interface_gateway_by_pci(pci):
     gw_ip, _ = get_interface_gateway('', if_pci=pci)
-    return ipaddress.ip_address(unicode(gw_ip))
+    return ipaddress.ip_address(gw_ip)
 
 
 def get_all_interfaces():
