f557f66948c8a766cf9be363ec9155d50a798c7a
Shneor Pinsky <=>
2021-02-22 14:54:09 +0000

    wip

--- tests/router/conftest.py
+++ tests/router/conftest.py
@@ -3,6 +3,9 @@ import os
 import glob
 import shutil
 import yaml
+import json
+import subprocess
+import re
 from datetime import datetime
 
 @pytest.fixture
@@ -64,18 +67,35 @@ def interface_3(request):
     return request.config.getoption('--interface-3')
 
 @pytest.fixture(autouse=True)
-def prepare_cli_files(currpath, interface_1, interface_2, interface_3):
-    tests_path = currpath.replace('.py', '')
+def prepare_cli_files(currpath):
+    vendor = subprocess.check_output("lshw -c system | awk '/vendor/{print tolower($2)}' 2>&1", shell=True).strip()
 
+    data = None
+    with open(os.path.abspath('./fwtests.yaml'), 'r') as stream:
+        info = yaml.load(stream, Loader=yaml.BaseLoader)
+        for env in info['devices']:
+            if vendor == env:
+                data = info['devices'][env]
+
+    tests_path = currpath.replace('.py', '')
     # get original tests files
     test_cases = sorted(glob.glob('%s/*.cli' % tests_path))
     expected_files = sorted(glob.glob('%s/*configuration_dump.json' % tests_path))
     original_files = test_cases + expected_files
-    os.path.realpath(__file__)
-    with open(self.CFG_FWAGENT_CONF_FILE, 'r') as f:
-        conf = yaml.load(f, Loader=yaml.SafeLoader)
-        if conf.get('agent') and conf['agent'].get('uuid'):
-            return True
+
+    def _replace(item):
+        # use template
+        if isinstance(item, str) or isinstance(item, unicode):
+            return data[item]
+
+        # use specific field
+        elif type(item) == dict:
+            for key, value in item.items():
+                match = re.search('(__INTERFACE_[1-3]__)(.*)', value)
+                if match:
+                    interface, field = match.groups()
+                    item[key] = data[interface][field]
+        return item
 
     for file in original_files:
         # copy original file and save
@@ -86,29 +106,47 @@ def prepare_cli_files(currpath, interface_1, interface_2, interface_3):
         shutil.copyfile(file, copy_path)
 
         # replace tests variables
-        # interface1_devId = yaml.load
+        with open(file, 'r+') as json_file:
+            requests = json.load(json_file)
+            for req in requests:
+                if not 'params' in req:
+                    continue
 
-        os.system("sed -i 's/%s/%s/g' %s" % ("__INTERFACE_1__", interface_1, file))
-        os.system("sed -i 's/%s/%s/g' %s" % ("__INTERFACE_2__", interface_2, file))
-        os.system("sed -i 's/%s/%s/g' %s" % ("__INTERFACE_3__", interface_3, file))
+                msg = req['message']
+                if msg == 'start-router':
+                    interfaces = req['params']['interfaces'] if 'interfaces' in req['params'] else None
+                    if interfaces:
+                        for idx, interface in enumerate(interfaces):
+                            updated_interface = _replace(interface)
+                            interfaces[idx] = updated_interface
 
-    yield
+                with open(file, 'w+') as json_file:
+                    json.dump(requests, json_file, sort_keys=True, indent=1)
+            # print(cfg)
+        
+        a = 'a'
 
-    # get modified files
-    modified_test_cases = sorted(glob.glob('%s/*.cli' % tests_path))
-    modified_expected_files = sorted(glob.glob('%s/*.json' % tests_path))
-    modified_files = modified_test_cases + modified_expected_files
+        # os.system("sed -i 's/%s/%s/g' %s" % ("__INTERFACE_1__", interface_1, file))
+        # os.system("sed -i 's/%s/%s/g' %s" % ("__INTERFACE_2__", interface_2, file))
+        # os.system("sed -i 's/%s/%s/g' %s" % ("__INTERFACE_3__", interface_3, file))
 
-    # remove all modified files
-    for file in modified_files:
-        os.remove(file)
+    # yield
 
-    # get original tests files and restored them
-    original_files = sorted(glob.glob('%s/*orig_*' % tests_path))
-    for file in original_files:
-        # copy original file and save
-        if 'orig_json' in file:
-            copy_path = file.replace('.orig_json', '.json')
-        else:
-            copy_path = file.replace('.orig_cli', '.cli')
-        shutil.move(file, copy_path)
+    # # get modified files
+    # modified_test_cases = sorted(glob.glob('%s/*.cli' % tests_path))
+    # modified_expected_files = sorted(glob.glob('%s/*.json' % tests_path))
+    # modified_files = modified_test_cases + modified_expected_files
+
+    # # remove all modified files
+    # for file in modified_files:
+    #     os.remove(file)
+
+    # # get original tests files and restored them
+    # original_files = sorted(glob.glob('%s/*orig_*' % tests_path))
+    # for file in original_files:
+    #     # copy original file and save
+    #     if 'orig_json' in file:
+    #         copy_path = file.replace('.orig_json', '.json')
+    #     else:
+    #         copy_path = file.replace('.orig_cli', '.cli')
+    #     shutil.move(file, copy_path)
