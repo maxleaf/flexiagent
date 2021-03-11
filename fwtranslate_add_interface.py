b194990803da8c58fa75994d68b344b7b59c086c
Shneor Pinsky <shneor521@gmail.com>
2021-03-11 11:49:08 +0000

    tap connet to create tap

--- fwtranslate_add_interface.py
+++ fwtranslate_add_interface.py
@@ -106,16 +106,17 @@ def add_interface(params):
     is_non_dpdk = is_wifi or is_lte
 
     if is_non_dpdk:
-        # create tap for this interface in vpp and linux
+        # Create tap interface in linux and vpp.
+        # This command will create three interfaces:
+        #   1. linux tap interface.
+        #   2. vpp tap interface in vpp.
+        #   3. linux interface for tap-inject.
+        #
         cmd = {}
         cmd['cmd'] = {}
-        cmd['cmd']['name']   = "python"
-        cmd['cmd']['params'] = {
-                    'module': 'fwutils',
-                    'func': 'configure_tap_in_linux_and_vpp',
-                    'args': { 'linux_if_name': iface_name }
-        }
-        cmd['cmd']['descr'] = "create tap interface in linux and vpp"
+        cmd['cmd']['name']    = "exec"
+        cmd['cmd']['descr']   = "create tap interface in vpp and linux"
+        cmd['cmd']['params']  = ["sudo vppctl create tap host-if-name %s" % fwutils.generate_linux_interface_short_name("tap", iface_name)]
         cmd_list.append(cmd)
 
         if is_wifi:
