39d09e86b0dcfebc83be9895ef5580f0fbd017b5
Shneor Pinsky <shneorp@flexiwan.com>
2021-03-16 11:15:45 +0000

    remove confused comment

--- fwagent.py
+++ fwagent.py
@@ -679,7 +679,6 @@ def reset(soft=False, quiet=False):
             reset_device = False
 
     if reset_device:
-        # If vpp is run, we need to stop the router, and reset the device configuration
         if fwutils.vpp_does_run():
             print("stopping the router...")
         daemon_rpc('stop', stop_router=True)
