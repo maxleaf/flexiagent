c2d70262bd6db1cb62d0f7f40c05ba78280a7b28
IgorN <igor@flexiwan.com>
2021-06-15 16:40:14 +0300

    Fix missing underscore in dump filename (the bug was introduced by one of latest fixes)

--- fwutils.py
+++ fwutils.py
@@ -4064,14 +4064,14 @@ def send_udp_packet(src_ip, src_port, dst_ip, dst_port, dev_name, msg):
 
     s.close()
 
-def build_timestamped_filename(filename, ext=''):
+def build_timestamped_filename(filename, ext='', separator='_'):
     '''Incorporates date and time into the filename in format "%Y%M%d_%H%M%S".
     Example:
         build_timestamped_filename("fwdump_EdgeDevice01_", ext='.tar.gz')
         ->
         fwdump_EdgeDevice01_20210510_131900.tar.gz
     '''
-    return filename + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ext
+    return filename + separator + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ext
 
 def is_ip(str_to_check):
     try:
