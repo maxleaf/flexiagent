49c0607a59808787b3b22473e0ef8bba132ddbdb
Shneor Pinsky <shneorp@flexiwan.com>
2021-10-04 09:52:47 +0000

    Add the last stats entry to get-device-info response

--- fwagent_api.py
+++ fwagent_api.py
@@ -161,9 +161,9 @@ class FWAGENT_API:
             # Load component versions
             with open(fwglobals.g.VERSIONS_FILE, 'r') as stream:
                 info = yaml.load(stream, Loader=yaml.BaseLoader)
+            info['stats'] = stats['message'][-1]
             # Load network configuration.
             info['network'] = {}
-            info['stats'] = stats['message'][-1]
             info['network']['interfaces'] = list(fwutils.get_linux_interfaces(cached=False).values())
             info['reconfig'] = '' if loadsimulator.g.enabled() else fwutils.get_reconfig_hash()
             if fwglobals.g.ikev2.is_private_key_created():
