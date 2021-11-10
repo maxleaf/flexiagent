7b3d4c561299383d4c7a493a55f9ac375c12d5cb
Shneor Pinsky <shneorp@flexiwan.com>
2021-11-10 17:45:38 +0200

    Fix auth script url

--- services/openvpn_scripts/auth.sh
+++ services/openvpn_scripts/auth.sh
@@ -3,10 +3,8 @@
 username=$(head -n 1 $1)
 password=$(cat $1 | head -2 | tail -1)
 
-url="https://localvpn.flexiwan.com:4443/api/auth/token?username=${username}"
-# url="http://local.vpnflexiwan.com:5000/api/auth/token?username=${username}"
+url="https://localvpn.flexiwan.com:4443/api/auth/tokens/verify?username=${username}"
 
-# response=$(curl -H "Authorization: Bearer ${password}" --insecure --write-out '%{http_code}' --silent --output /dev/null $url)
 response=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${password}" -d @/etc/flexiwan/agent/fwagent_info.txt --insecure --write-out '%{http_code}' --silent --output /dev/null $url)
 
 if [[ "$response" -ne 200 ]] ; then
